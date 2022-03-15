51509048ed6598cdf52045384e92f29f3a20096a
Abu Sayeed <abusayeed33343536@gmail.com>
2022-03-15 21:18:42 +0600

    test2

--- app/Http/Controllers/TimeKeeperController.php
+++ app/Http/Controllers/TimeKeeperController.php
@@ -19,28 +19,7 @@ class TimeKeeperController extends Controller
         return view('pages.Admin.timekeeper.index',compact('employees','projects','clients'));
     }
 
-    // public function store(Request $request){
-    //     TimeKeeper::insert([
-    //         "user_id" => Auth::id(),
-    //         "employeeID" => $request->employeeID,
-    //         "clientID" => $request->clientID,
-    //         "projectID" => $request->projectID,
-    //         "projectStartDate" => $request->projectStartDate,
-    //         "projectEndDate" => $request->projectEndDate,
-    //         "roasterStartDate" => $request->roasterStartDate,
-    //         "roasterEndDate" => $request->roasterEndDate,
-    //         "duration" => $request->duration,
-    //         "ratePerHour" => $request->ratePerHour,
-    //         "amount" => $request->amount,
-    //         "remarks" => $request->remarks,
-    //         "created_at" => Carbon::now()
-    //     ]);
-    //     $notification=array(
-    //         'message'=>'Roaster Added Successfully !!!',
-    //         'alert-type'=>'success'
-    //     );
-    //     return Redirect()->route('index')->with($notification);
-    // }
+    
 
     public function storeTimeKeeper(Request $request)
     {
